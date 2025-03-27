5123b3c8dbec45c757160c8dd6ccc8cd0335e285
lucienchizungu <lucienchizungu05@gmail.com>
2025-03-27 18:40:46 +0200

    first commit

---
+++ TP2/Connection/dbConnection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+
+namespace TP2.Connection
+{
+    class dbConnection
+    {
+
+        public SqlConnection GetSqlConnection()
+        {
+            return new SqlConnection("Data source=DESKTOP-IALFIS6;Initial catalog=db_person;User=sa;password=luc");
+        }
+
+        public MySqlConnection GetMySqlConnection()
+        {
+            return new MySqlConnection("Datasource=localhost;port=3306;Initial catalog=db_person;username=root;password=");
+        }
+    }
+}
